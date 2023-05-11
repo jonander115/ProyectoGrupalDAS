488547e795dc18ab53d664a5101d0138edf4fe1e
Jon Ander López de Ahumada <jlopezdeahumada@gmail.com>
2023-05-11 20:39:32 +0200

    Commit perfil usuario

--- app/src/main/java/com/example/proyectogrupaldas/MainActivity.java
+++ app/src/main/java/com/example/proyectogrupaldas/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.proyectogrupaldas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //BORRAR
+        Intent i = new Intent (this, PerfilUsuario.class);
+        i.putExtra("usuario","joni");
+        startActivity(i);
     }
 }
