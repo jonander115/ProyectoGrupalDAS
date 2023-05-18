6dfee13956a96708e94440c9ea0e1ac04554e33c
Dimitlink <julia.patrycja.wojciechowska@gmail.com>
2023-05-18 20:54:47 +0200

    Estadisticas

---
+++ app/src/main/java/com/example/proyectogrupaldas/MenuEstadisticas.java
@@ -0,0 +1,51 @@
+package com.example.proyectogrupaldas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MenuEstadisticas extends AppCompatActivity {
+
+    String usuario = "joni";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_estadisticas);
+
+
+        ListView listView = findViewById(R.id.listaMenuEstadisticas);
+        ArrayList<String> listaOpciones = new ArrayList<>();
+        listaOpciones.add("Rutinas");
+        listaOpciones.add("Ejercicios");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listaOpciones);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Acciones a realizar cuando se hace clic en un elemento de la lista
+                String opcionSeleccionada = listaOpciones.get(position);
+                if(opcionSeleccionada.equals("Rutinas")){
+                    Intent intent = new Intent(MenuEstadisticas.this, RutinasEstadistica.class);
+                    intent.putExtra("usuario", usuario);
+                    startActivity(intent);
+
+                }else{
+                    Intent intent = new Intent(MenuEstadisticas.this, EjerciciosEstadisticas.class);
+                    intent.putExtra("usuario", usuario);
+                    startActivity(intent);
+                }
+                finish();
+            }});
+
+    }
+}
